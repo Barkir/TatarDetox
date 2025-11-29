077d244d769f9760074195b69083954fd1ed1a37
Barkir <r.barinov2006@gmail.com>
2025-11-29 23:05:48 +0300

    gemini_kill_feature

---
+++ Algorithm-Kirill/gemini_kill_feature/create_tsv.py
@@ -0,0 +1,57 @@
+import json
+import csv
+from similar_text import similar_text
+
+TSV_ORIGINAL = "dev_inputs.tsv"
+TOXIC_WORDS = "output_results_1.json"
+OUTPUT = "output.tsv"
+
+
+def detoxify_message(msg: str) -> str:
+    if not msg.strip():  # если сообщение пустое — вернуть как есть
+        return msg
+    print(msg)
+
+
+
+
+with open(TOXIC_WORDS, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+
+msg = detoxify_message(" ".join(data[15]["0"][1:]))
+
+
+
+modified_rows = []
+
+# with open(TSV_ORIGINAL, encoding="utf-8-sig") as fd:
+#     rd = csv.reader(fd, delimiter="\t")
+#     header = next(rd)  # читаем заголовок
+#     print(header)
+#     # Ожидаем: ['ID', 'tat_toxic']
+#     # assert len(header) == 2 and header[0] == "ID" and header[1] == "tat_toxic", \
+#         # "Unexpected header in input TSV"
+#
+#     # Добавляем третий столбец
+#     new_header = ["ID", "tat_toxic", "tat_detox1"]
+#     modified_rows.append(new_header)
+#
+#     for row in rd:
+#         row_id = row[0]
+#         msg = row[1]
+#
+#         detox_msg = detoxify_message(msg, data)
+#
+#         # Гарантируем, что нет пустого значения
+#         if not detox_msg.strip():
+#             detox_msg = msg
+#
+#         modified_rows.append([row_id, msg, detox_msg])
+#
+# # Запись результата
+# with open(OUTPUT, "w", encoding="utf-8", newline='') as f:
+#     writer = csv.writer(f, delimiter="\t", quoting=csv.QUOTE_NONE, escapechar='\\')
+#     writer.writerows(modified_rows)
+
+# print(f"✅ {len(modified_rows) - 1} rows processed. Saved to {OUTPUT}")
